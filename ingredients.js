a860c45e10b38ea3663f7128c3df781588fdae1a
John Sweeney <jes2806@gmail.com>
2022-07-11 14:47:39 -0600

    fixed a code in ingredients.js

--- ingredients.js
+++ ingredients.js
@@ -1,11 +1,6 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
 // Write a while loop that prints out the contents of ingredients:
-for (let i = 0; i < ingredients.length; i++);
-
-console.log(ingredients);
-
-// Write a for loop that prints out the contents of ingredients:
 let i = 0;
 while (i < ingredients.length) {
 
@@ -14,7 +9,12 @@ while (i < ingredients.length) {
   i++;
 }
 
-// Write any loop (while or for) that prints out the contents of ingredients backwards:
-for (let i = 0; i < ingredients.length; i++);
+// Write a for loop that prints out the contents of ingredients:
+for (let i = 0; i < ingredients.length; i++){
+  console.log(ingredients[i]);
+  }
 
-console.log(ingredients.reverse());
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+  for (let i = ingredients.length - 1; i >= 0; i--) {
+    console.log(ingredients[i]);
+  };
